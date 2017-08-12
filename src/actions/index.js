24bb05f6fac326a20ca626871fc4e27308485617
humdrum <jjrajani@gmail.com>
2017-08-11 20:09:07 -0400

    add axios to project.  make get call for weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = `3c0d064bb138de55e8eca0016231961a`;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,9 +6,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
-    payload: url,
+    payload: request,
   }
 }
