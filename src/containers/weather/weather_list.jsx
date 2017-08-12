c3279432ede56beb1f93ccfddcbe00661da5e7ef
humdrum <jjrajani@gmail.com>
2017-08-12 09:27:20 -0400

    added weather city name to weather list table.  installed react-sparklines for the upcoming graph implementation

--- src/containers/weather/weather_list.jsx
+++ src/containers/weather/weather_list.jsx
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    console.log(this.props.weather);
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
+
+  renderWeather(weather, i) {
+    return (
+      <tr key={i}>
+        <td>{weather.city.name}</td>
+      </tr>
+    );
+  }
+
 }
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
