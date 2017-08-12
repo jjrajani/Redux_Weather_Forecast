c3bf22f5b642d4c8132798975a9c495389d8f706
humdrum <jjrajani@gmail.com>
2017-08-12 10:31:47 -0400

    styles for chart legend. convert values to standard display units

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import _ from 'lodash';
 import {
   Sparklines,
   SparklinesLine,
   SparklinesReferenceLine
  } from 'react-sparklines';
 
-export default ({ data, color }) => (
-  <div>
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+  // return Math.round(data.reduce((a, b) => a + b) / data.length);
+}
+
+export default ({ data, color, unit }) => (
+  <div className="chart">
     <Sparklines data={data} width={180} height={120} >
       <SparklinesLine color={color} />
       <SparklinesReferenceLine type="avg" />
     </Sparklines>
+    <div className="legend">{average(data)}<span className="unit">{unit}</span></div>
   </div>
 );
--- src/containers/weather/weather_list.jsx
+++ src/containers/weather/weather_list.jsx
@@ -25,17 +25,27 @@ class WeatherList extends Component {
     );
   }
 
-  renderWeather(weather, i) {
-    const temps = weather.list.map(w => w.main.temp);
-    const pressures = weather.list.map(w => w.main.pressure);
+  kelvinToFahrenheit(temp) {
+    /* F */
+    return (temp * (9/5) - 459.67);
+  }
+
+  hectopascalToInMG(pressure) {
+    /* inHg */
+    return 0.02952998751 * pressure;
+  }
+
+  renderWeather = (weather, i) => {
+    const temps = weather.list.map(w => this.kelvinToFahrenheit(w.main.temp));
+    const pressures = weather.list.map(w => this.hectopascalToInMG(w.main.pressure));
     const humidities = weather.list.map(w => w.main.humidity);
 
     return (
       <tr key={i}>
         <td>{weather.city.name}</td>
-        <td><Chart color="orange" data={temps}/></td>
-        <td><Chart color="purple" data={pressures}/></td>
-        <td><Chart color="blue" data={humidities}/></td>
+        <td><Chart color="orange" data={temps} unit="F°"/></td>
+        <td><Chart color="purple" data={pressures} unit="inHG"/></td>
+        <td><Chart color="blue" data={humidities} unit="%"/></td>
       </tr>
     );
   }
